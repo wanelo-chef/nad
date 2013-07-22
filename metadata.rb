1deaef0982d810dd0eca88a9267decc6f0e43857
Blake Irvin <pair+blake@wanelo.com>
2013-07-22 11:19:40 -0700

    Better private ip assignment for Linux hosts

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "b.irvin@modcloth.com"
 license          "Apache 2.0"
 description      "Installs/Configures nad"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.1"
+version          "0.4.2"
 
 depends "ipaddr_extensions"
 suggests "smf"
