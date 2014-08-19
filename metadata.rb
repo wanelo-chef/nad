bee17f2d6a3cc6d27b95855c94b0321d9005e45d
Doc Ritezel & James Hart <doc+james@wanelo.com>
2014-08-19 16:49:15 -0700

    Fix a typo in the haproxy check

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-maintainer       "ModCloth, Inc."
-maintainer_email "b.irvin@modcloth.com"
-license          "Apache 2.0"
-description      "Installs/Configures nad"
+maintainer       'ModCloth, Inc.'
+maintainer_email 'b.irvin@modcloth.com'
+license          'Apache 2.0'
+description      'Installs/Configures nad'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.5.0"
+version          '0.5.1'
 
 depends 'ipaddr_extensions'
 depends 'nodejs'
